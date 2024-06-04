5a86f31ef818a2d1756c46dbed65be65cb0ff1c7
VEDANT ACHHARYA <achharyavedant@gmail.com>
2024-06-04 20:51:29 +0530

    Update Avatar.tsx

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -13,7 +13,7 @@ const Avatar: React.FC<AvatarProps> = ({ src }) => {
       height="30" 
       width="30" 
       alt="Avatar" 
-      src={src || '/images/placeholder.jpg'}
+      src={src || '../avatar.png'}
     />
    );
 }
